2a8b0e782a4bc2803c78a9f63d92d9229bb4cbce
raniazyane <rania.zyane@gmail.com>
2017-10-21 22:39:50 +0100

    create routes and default layout

---
+++ src/routes.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import { Link } from 'react-router-dom'
+
+import DefaultLayout from './Layout'
+
+import Login from './pages/Login'
+import Home from './pages/Home'
+import About from './pages/About'
+
+export default function MainRouter() {
+    return (
+        <Router>
+            <div>
+                <DefaultLayout exact path="/" component={Home} />
+                <DefaultLayout path="/about" component={About} />
+                <Route path="/login" component={Login} />
+            </div>
+        </Router>
+    )
+}
